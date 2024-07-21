cef9b6f0b15dbfa06161f7da02e51623400dd7d2
curso-programacion-cet <lmelamed@chicasentecnologia.org>
2024-07-21 19:24:24 -0300

    preparo la rama para ser deployada como sitio web

--- js/inicio.js
+++ js/inicio.js
@@ -6,7 +6,7 @@ botonComenzar.addEventListener("click", () => {
     // Podemos utilizar la propiedad location (ubicación) del objeto window (ventana)
     // para cambiar algunas propiedades de la ubicación (podemos redirigir a otro sitio web o al nuestro mismo)
     // Pueden escribir en la consola del navegador window.location para ver que propiedades tiene
-    window.location.href = "../html/cuidados.html"; // similar al tag "<a>"
+    window.location.href = "cuidados.html"; // similar al tag "<a>"
 });
 
 
